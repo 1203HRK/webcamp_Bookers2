bd9ca74b5f5cbb289cd87fdb1b7b99a874f45232
1203HRK <14haaru05@gmail.com>
2021-01-19 10:50:49 +0000

    4-10[Add]

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
+  get 'home/about' => "homes#index"
   resources :books, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit, :update]
 end
