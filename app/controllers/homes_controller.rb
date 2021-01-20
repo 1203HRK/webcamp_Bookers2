bd9e8a24c7d08623d738b24bedd4e1ec901fffd6
1203HRK <14haaru05@gmail.com>
2021-01-20 03:03:30 +0000

    4-10[Add]

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,4 +1,5 @@
 class HomesController < ApplicationController
+  before_action :authenticate_user!,except: [:index,]
 
   def top
   end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
+  
 
+  def index
+    @users = User.all
+  end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(current_user.id)
     @book = Book.new
     @books = @user.books
   end
