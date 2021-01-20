e5c119e93b713710b553111f23f5e6f4a7c2e010
1203HRK <14haaru05@gmail.com>
2021-01-20 04:59:06 +0000

    4-10[Add]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -8,7 +8,7 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     if @book.save
-    redirect_to books_path
+    redirect_to book_path(@book.id)
     else
       render :new
     end
@@ -22,7 +22,20 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
   def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
